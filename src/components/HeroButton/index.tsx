4c6f17ce1d269876e480bb250614bdb726da82b7
Harkesh Yadav <raoharkesh1998@gmail.com>
2023-12-17 23:18:11 +0530

    link comp added

--- src/components/HeroButton/index.tsx
+++ src/components/HeroButton/index.tsx
@@ -1,15 +1,21 @@
+import React from 'react'
 import styles from './styles.module.css'
+import Link from 'next/link'
 
-const HeroButton = () =>{
+interface Props {
+    path: string
+}
+
+const HeroButton: React.FC<Props> = ({path}) =>{
     return (
         // <div className={styles.buttonWrap}>
         //     <button className={styles.heroButton}>All Products</button>         
         // </div>
-        <>
+        <Link href={path}>
         <button className={styles.heroButton}>All Products
         <img className={styles.arrowImg} src='./circle-with-arrow.png' />
         </button>  
-        </>
+        </Link>
     )
 }
 
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from './styles.module.css'
+import Link from "next/link";
 
 const Navbar = () =>{
     return (
         <>
         <div className={styles.navouter}>
             <div className={styles.navinner}>
+                <Link href='/'>
                 <img className={styles.navlogo} src="logo.jpg" />
+                </Link>
                 <div className={styles.navinnerright}>
                     <div className={styles.navbuttonwrap}>
                         <button className={styles.navbuttonprimary}>Home</button>         
