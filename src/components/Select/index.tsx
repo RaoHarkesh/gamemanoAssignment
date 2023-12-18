62c2f0c25541d23f0d690ff903268d489491ae1b
Harkesh <raoharkesh1998@gmail.com>
2023-12-18 15:22:53 +0530

    categories fetched

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,20 +1,43 @@
 "use client"
 import React , { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.css'
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation'
 
-const StyledSelect = () =>{
-    const categories = ['All Products', 'Headphones', 'Mouse', 'Keyboard'];
+interface Props {
+  handleFilterChange: Function
+}
+
+const StyledSelect: React.FC<Props> = ({handleFilterChange}) =>{
+    const [categories, setCategories] = useState<Array<string>>([]);
     const [active, setActive] = useState(categories[0])
     const [open, setOpen] = useState<boolean>(false)
     const selectDiv = useRef<HTMLDivElement>(null);
+    const params = useSearchParams();
+    const category = params.get('category');
+
+    const getCategories = async () =>{
+      try{
+        const response = await fetch('https://dummyjson.com/products/categories');
+        const data = await response.json();
+        setCategories(data);
+        setActive(data[0])
+      }catch{
+
+      }
+  
+    }
+
 
     useEffect(() => {
+      console.log("run")
         const handleClickOutside = (event) => {
           if (selectDiv.current && !selectDiv.current.contains(event.target)) {
             setOpen(false);
           }
         };
-    
+
+        getCategories();
         document.addEventListener('click', handleClickOutside);
     
         return () => {
@@ -22,11 +45,26 @@ const StyledSelect = () =>{
         };
       }, []);
 
+      useEffect(()=>{
+        if(category){
+          handleFilterChange(category);
+        }else{
+          handleFilterChange('')
+        }
+      },[category])
+
+    
+
     return (
         <div className={styles.selectWrapper}  ref={selectDiv} onClick={()=>setOpen(!open)}>
-            <div className={styles.selectItem}>{active}</div>
+            <div className={`${styles.selectItem} ${styles.activeWrapper}`}>{active}
+                <img src='down-arrow.png' className={`${styles.arrowImage} ${open && styles.reverse}`} />
+            </div>
             <div className={`${styles.optionWrapper} ${open ? 'block' : 'hidden'}`}>
-            {categories.filter((ele)=>ele!== active).map((ele)=><div key={ele} onClick={()=>setActive(ele)} className={styles.selectItem}>{ele}</div>)}
+            {categories.filter((ele)=> ele !== active).map((ele)=><Link key={ele} href={{
+            pathname: '/products',
+            query: { category: ele.toLocaleLowerCase() },
+          }}><div key={ele} onClick={()=>setActive(ele)} className={styles.selectItem}>{ele}</div></Link>)}
             </div>
         </div>
     )
