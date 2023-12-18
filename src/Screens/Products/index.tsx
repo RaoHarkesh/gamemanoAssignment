aa942502c45143e70900cfd6959d9ea65fc06ac7
Harkesh <raoharkesh1998@gmail.com>
2023-12-18 15:18:43 +0530

    handle change filter added

--- src/Screens/Products/index.tsx
+++ src/Screens/Products/index.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import Hero from '@/components/HeroSection'
 import styles from './styles.module.css'
 import ProductCard from '@/components/ProductCard';
 import StyledSelect from '@/components/Select';
+import { useState } from 'react';
 
 interface Props {
     products: Array<Product> | null
@@ -16,7 +19,19 @@ interface Product {
     images: Array<string>
 }
 
-const Products: React.FC<Props> = ({products}) =>{
+const Products: React.FC<Props> = () =>{
+    const [products, setProducts] = useState<Array<Product>>([]);
+    const handleChange = async (category: string) =>{
+        try{
+            const url = category === "" ? 'https://dummyjson.com/products' : `https://dummyjson.com/products/category/${category}`
+            const response = await fetch(url);
+            const data = await response.json();
+            setProducts(data.products);
+        }catch{
+
+        }
+
+    }
     return (
         <>
         <Hero>
@@ -26,16 +41,17 @@ const Products: React.FC<Props> = ({products}) =>{
             </div>
             </>
         </Hero>
-        <div className={`mw-1300 bg-theme-purple ${styles.screenWrapper}`}>
+        <div className='bg-theme-purple'>
+        <div className={`mw-1300  ${styles.screenWrapper}`}>
             <div className={styles.filterSection}>
                 <h3 className={styles.breadCrumb}>{'>> All Products'}</h3>
-                <StyledSelect/>
+                <StyledSelect handleFilterChange={handleChange} />
             </div>
             <div className={styles.gridWrap}>
             {products?.map((ele)=><ProductCard key={ele.id} id={ele.id} thumbnail={ele.images[1]} title={ele.title} />)}
             </div>
         </div>
-        
+        </div>
         </>
     )
 }
